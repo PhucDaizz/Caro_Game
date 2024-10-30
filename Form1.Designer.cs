cdb9677ce6ddcea1d9dd03ddc2add4240c696d78
ThanhDatis <2251120340@ut.edu.vn>
2024-10-30 21:24:16 +0700

    Xuống phiên bản .net8 thành .net6, hoàn thành socket chơi trực tuyến

---
+++ Form1.Designer.cs
@@ -0,0 +1,315 @@
+﻿namespace Game_Caro
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            pnlChessBeard = new Panel();
+            panel2 = new Panel();
+            pctbAvatar = new PictureBox();
+            pctbXO = new PictureBox();
+            lbNameXO = new Label();
+            lbRole = new Label();
+            panel3 = new Panel();
+            lbLoading = new Label();
+            btnLan = new Button();
+            prcbCoolDown = new ProgressBar();
+            txbIP = new TextBox();
+            txbPlayerName = new TextBox();
+            pctbMark = new PictureBox();
+            tmCoolDown = new System.Windows.Forms.Timer(components);
+            menuStrip1 = new MenuStrip();
+            menuToolStripMenuItem = new ToolStripMenuItem();
+            newGameToolStripMenuItem = new ToolStripMenuItem();
+            undoToolStripMenuItem = new ToolStripMenuItem();
+            quitToolStripMenuItem = new ToolStripMenuItem();
+            toolStripMenuItem1 = new ToolStripMenuItem();
+            panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pctbAvatar).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pctbXO).BeginInit();
+            panel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pctbMark).BeginInit();
+            menuStrip1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // pnlChessBeard
+            // 
+            pnlChessBeard.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            pnlChessBeard.BackColor = SystemColors.Control;
+            pnlChessBeard.Location = new Point(15, 34);
+            pnlChessBeard.Margin = new Padding(4);
+            pnlChessBeard.Name = "pnlChessBeard";
+            pnlChessBeard.Size = new Size(953, 682);
+            pnlChessBeard.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            panel2.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
+            panel2.BackColor = SystemColors.ButtonHighlight;
+            panel2.Controls.Add(pctbAvatar);
+            panel2.Controls.Add(pctbXO);
+            panel2.Controls.Add(lbNameXO);
+            panel2.Controls.Add(lbRole);
+            panel2.Location = new Point(1086, 39);
+            panel2.Margin = new Padding(4);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(142, 109);
+            panel2.TabIndex = 0;
+            panel2.Paint += panel2_Paint;
+            // 
+            // pctbAvatar
+            // 
+            pctbAvatar.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            pctbAvatar.BackgroundImage = (Image)resources.GetObject("pctbAvatar.BackgroundImage");
+            pctbAvatar.BackgroundImageLayout = ImageLayout.Stretch;
+            pctbAvatar.Location = new Point(8, 2);
+            pctbAvatar.Margin = new Padding(4);
+            pctbAvatar.Name = "pctbAvatar";
+            pctbAvatar.Size = new Size(134, 109);
+            pctbAvatar.SizeMode = PictureBoxSizeMode.StretchImage;
+            pctbAvatar.TabIndex = 0;
+            pctbAvatar.TabStop = false;
+            pctbAvatar.Click += pctbAvatar_Click;
+            // 
+            // pctbXO
+            // 
+            pctbXO.BackColor = SystemColors.ButtonHighlight;
+            pctbXO.Location = new Point(82, 111);
+            pctbXO.Margin = new Padding(4);
+            pctbXO.Name = "pctbXO";
+            pctbXO.Size = new Size(31, 31);
+            pctbXO.SizeMode = PictureBoxSizeMode.StretchImage;
+            pctbXO.TabIndex = 4;
+            pctbXO.TabStop = false;
+            // 
+            // lbNameXO
+            // 
+            lbNameXO.AutoSize = true;
+            lbNameXO.Font = new Font("Microsoft Sans Serif", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            lbNameXO.Location = new Point(8, 111);
+            lbNameXO.Margin = new Padding(4, 0, 4, 0);
+            lbNameXO.Name = "lbNameXO";
+            lbNameXO.Size = new Size(0, 22);
+            lbNameXO.TabIndex = 4;
+            // 
+            // lbRole
+            // 
+            lbRole.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            lbRole.AutoSize = true;
+            lbRole.Font = new Font("Microsoft Sans Serif", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            lbRole.ForeColor = Color.Crimson;
+            lbRole.Location = new Point(25, 2);
+            lbRole.Margin = new Padding(4, 0, 4, 0);
+            lbRole.Name = "lbRole";
+            lbRole.Size = new Size(0, 29);
+            lbRole.TabIndex = 4;
+            // 
+            // panel3
+            // 
+            panel3.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+            panel3.BackColor = SystemColors.ControlLightLight;
+            panel3.Controls.Add(lbLoading);
+            panel3.Controls.Add(btnLan);
+            panel3.Controls.Add(prcbCoolDown);
+            panel3.Controls.Add(txbIP);
+            panel3.Controls.Add(txbPlayerName);
+            panel3.Location = new Point(976, 154);
+            panel3.Margin = new Padding(4);
+            panel3.Name = "panel3";
+            panel3.Size = new Size(254, 562);
+            panel3.TabIndex = 0;
+            // 
+            // lbLoading
+            // 
+            lbLoading.AutoSize = true;
+            lbLoading.Location = new Point(444, 62);
+            lbLoading.Margin = new Padding(4, 0, 4, 0);
+            lbLoading.Name = "lbLoading";
+            lbLoading.Size = new Size(0, 25);
+            lbLoading.TabIndex = 4;
+            // 
+            // btnLan
+            // 
+            btnLan.Location = new Point(17, 101);
+            btnLan.Margin = new Padding(4);
+            btnLan.Name = "btnLan";
+            btnLan.Size = new Size(227, 36);
+            btnLan.TabIndex = 3;
+            btnLan.Text = "Kết nối";
+            btnLan.UseVisualStyleBackColor = true;
+            btnLan.Click += btnLan_Click;
+            // 
+            // prcbCoolDown
+            // 
+            prcbCoolDown.Location = new Point(14, 43);
+            prcbCoolDown.Margin = new Padding(4);
+            prcbCoolDown.Name = "prcbCoolDown";
+            prcbCoolDown.Size = new Size(230, 11);
+            prcbCoolDown.TabIndex = 1;
+            prcbCoolDown.Click += prcbCoolDown_Click;
+            // 
+            // txbIP
+            // 
+            txbIP.Location = new Point(15, 62);
+            txbIP.Margin = new Padding(4);
+            txbIP.Name = "txbIP";
+            txbIP.Size = new Size(229, 31);
+            txbIP.TabIndex = 0;
+            txbIP.Text = "127.0.0.1";
+            txbIP.TextAlign = HorizontalAlignment.Center;
+            // 
+            // txbPlayerName
+            // 
+            txbPlayerName.Location = new Point(15, 4);
+            txbPlayerName.Margin = new Padding(4);
+            txbPlayerName.Name = "txbPlayerName";
+            txbPlayerName.ReadOnly = true;
+            txbPlayerName.Size = new Size(229, 31);
+            txbPlayerName.TabIndex = 0;
+            txbPlayerName.TextChanged += txbPlayerName_TextChanged;
+            // 
+            // pctbMark
+            // 
+            pctbMark.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            pctbMark.Location = new Point(974, 41);
+            pctbMark.Margin = new Padding(4);
+            pctbMark.Name = "pctbMark";
+            pctbMark.Size = new Size(104, 107);
+            pctbMark.SizeMode = PictureBoxSizeMode.StretchImage;
+            pctbMark.TabIndex = 2;
+            pctbMark.TabStop = false;
+            pctbMark.Click += pctbMark_Click;
+            // 
+            // tmCoolDown
+            // 
+            tmCoolDown.Tick += tmCoolDown_Tick;
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.ImageScalingSize = new Size(20, 20);
+            menuStrip1.Items.AddRange(new ToolStripItem[] { menuToolStripMenuItem, toolStripMenuItem1 });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Padding = new Padding(8, 2, 0, 2);
+            menuStrip1.Size = new Size(1233, 33);
+            menuStrip1.TabIndex = 1;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // menuToolStripMenuItem
+            // 
+            menuToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { newGameToolStripMenuItem, undoToolStripMenuItem, quitToolStripMenuItem });
+            menuToolStripMenuItem.Name = "menuToolStripMenuItem";
+            menuToolStripMenuItem.Size = new Size(73, 29);
+            menuToolStripMenuItem.Text = "Menu";
+            // 
+            // newGameToolStripMenuItem
+            // 
+            newGameToolStripMenuItem.Name = "newGameToolStripMenuItem";
+            newGameToolStripMenuItem.ShortcutKeys = Keys.Control | Keys.N;
+            newGameToolStripMenuItem.Size = new Size(270, 34);
+            newGameToolStripMenuItem.Text = "New Game";
+            newGameToolStripMenuItem.Click += newGameToolStripMenuItem_Click;
+            // 
+            // undoToolStripMenuItem
+            // 
+            undoToolStripMenuItem.Name = "undoToolStripMenuItem";
+            undoToolStripMenuItem.ShortcutKeys = Keys.Control | Keys.Z;
+            undoToolStripMenuItem.Size = new Size(270, 34);
+            undoToolStripMenuItem.Text = "Undo";
+            undoToolStripMenuItem.Click += undoToolStripMenuItem_Click;
+            // 
+            // quitToolStripMenuItem
+            // 
+            quitToolStripMenuItem.Name = "quitToolStripMenuItem";
+            quitToolStripMenuItem.ShortcutKeys = Keys.Control | Keys.Shift | Keys.E;
+            quitToolStripMenuItem.Size = new Size(270, 34);
+            quitToolStripMenuItem.Text = "Quit";
+            quitToolStripMenuItem.Click += quitToolStripMenuItem_Click;
+            // 
+            // toolStripMenuItem1
+            // 
+            toolStripMenuItem1.Name = "toolStripMenuItem1";
+            toolStripMenuItem1.Size = new Size(16, 29);
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1233, 749);
+            Controls.Add(panel3);
+            Controls.Add(panel2);
+            Controls.Add(pctbMark);
+            Controls.Add(pnlChessBeard);
+            Controls.Add(menuStrip1);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            MainMenuStrip = menuStrip1;
+            Margin = new Padding(4);
+            Name = "Form1";
+            Text = "Game Caro Fun ( Line up 5 )";
+            FormClosing += Form1_FormClosed;
+            Shown += Form1_Shown;
+            panel2.ResumeLayout(false);
+            panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pctbAvatar).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pctbXO).EndInit();
+            panel3.ResumeLayout(false);
+            panel3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pctbMark).EndInit();
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Panel pnlChessBeard;
+        private Panel panel2;
+        private Panel panel3;
+        private PictureBox pctbAvatar;
+        private PictureBox pctbMark;
+        private ProgressBar prcbCoolDown;
+        private TextBox textBox2;
+        private TextBox txbPlayerName;
+        private Button btnLan;
+        private PictureBox pictureBox2;
+        private TextBox txbIP;
+        private Label lbRole;
+        private System.Windows.Forms.Timer tmCoolDown;
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem menuToolStripMenuItem;
+        private ToolStripMenuItem toolStripMenuItem1;
+        private ToolStripMenuItem newGameToolStripMenuItem;
+        private ToolStripMenuItem undoToolStripMenuItem;
+        private ToolStripMenuItem quitToolStripMenuItem;
+        private Label lbNameXO;
+        private PictureBox pctbXO;
+        private Label lbLoading;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,287 @@
+﻿using System.Net.NetworkInformation;
+using System.Windows.Forms;
+
+namespace Game_Caro
+{
+    public partial class Form1 : Form
+    {
+        #region Properties
+        ChessBoardManager ChessBoard;
+
+        SocketManager socket;
+        #endregion
+        public Form1()
+        {
+            InitializeComponent();
+            Control.CheckForIllegalCrossThreadCalls = false;
+
+            ChessBoard = new ChessBoardManager(pnlChessBeard, txbPlayerName, pctbMark);
+            ChessBoard.EndedGame += ChessBoard_EndedGame;
+            ChessBoard.PlayerMarked += ChessBoard_PlayerMarked;
+
+            prcbCoolDown.Step = Constant.COOL_DOWN_STEP;
+            prcbCoolDown.Maximum = Constant.COOL_DOWN_TIME;
+            prcbCoolDown.Value = 0;
+            tmCoolDown.Interval = Constant.COOL_DOWN_INTERVAL;
+
+            socket = new SocketManager();
+
+            NewGame();
+        }
+
+        #region
+        void EndGame()
+        {
+            tmCoolDown.Stop();
+            pnlChessBeard.Enabled = false;
+            undoToolStripMenuItem.Enabled = false;
+        }
+        void ChessBoard_PlayerMarked(object? sender, ButtonClickEvent e)
+        {
+            tmCoolDown.Stop();
+            pnlChessBeard.Enabled = false;
+            prcbCoolDown.Value = 0;
+
+            socket.Send(new SocketData((int)SocketComand.SEND_POINT, "", e.ClickedPoint));
+            undoToolStripMenuItem.Enabled = false;
+            Listen();
+        }
+
+        void ChessBoard_EndedGame(object sender, EventArgs e)
+        {
+            EndGame();
+            socket.Send(new SocketData((int)SocketComand.END_GAME_LOSS, "", new Point()));
+
+        }
+
+        private void tmCoolDown_Tick(object sender, EventArgs e)
+        {
+            prcbCoolDown.PerformStep();
+            if (prcbCoolDown.Value >= prcbCoolDown.Maximum)
+            {
+                EndGame();
+                socket.Send(new SocketData((int)SocketComand.END_GAME_WIN, "", new Point()));
+                CustomMessageBox message = new CustomMessageBox("Bạn đã thua!", Color.Green);
+                message.ShowDialog();
+            }
+        }
+
+        void NewGame()
+        {
+            tmCoolDown.Stop();
+            prcbCoolDown.Value = 0;
+            undoToolStripMenuItem.Enabled = true;
+            ChessBoard.DrawChessBoard();
+
+        }
+
+        void Quit()
+        {
+            Application.Exit();
+        }
+
+        void Undo()
+        {
+            prcbCoolDown.Value = 0;
+            ChessBoard.Undo();
+        }
+
+        private void newGameToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            NewGame();
+            lbRole.Enabled = false;
+            lbRole.Text = "Host";
+            lbNameXO.Text = "You are ";
+            pctbXO.Image = Image.FromFile(Application.StartupPath + "\\Resources\\X_caro.jpg");
+            pctbXO.BackgroundImageLayout = ImageLayout.Stretch;
+            pnlChessBeard.Enabled = true;
+
+            socket.Send(new SocketData((int)SocketComand.NEW_GAME, "", new Point()));
+        }
+
+        private void undoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Undo();
+            socket.Send(new SocketData((int)SocketComand.UNDO, "", new Point()));
+        }
+
+        private void quitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Quit();
+        }
+
+        private void Form1_FormClosed(object sender, FormClosingEventArgs e)
+        {
+            if (MessageBox.Show("Bạn có chắc muốn thoát game không?", "Notification", MessageBoxButtons.OKCancel) != System.Windows.Forms.DialogResult.OK)
+                e.Cancel = true;
+            else
+            {
+                try
+                {
+                    socket.Send(new SocketData((int)SocketComand.QUIT, "", new Point()));
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+            }
+        }
+
+        private void btnLan_Click(object sender, EventArgs e)
+        {
+            socket.IP = txbIP.Text;
+            if (!socket.ConnectServer())
+            {
+                //prcbCoolDown.Maximum = Constant.COOL_DOWN_TIME1MINUTE;
+                //tmCoolDown.Start();
+                //lbLoading.Text = "Đang chờ đối thủ";
+                socket.isServer = true;
+                pnlChessBeard.Enabled = true;
+                socket.CreateServer();
+                lbRole.Enabled = false;
+                lbRole.Text = "Host";
+                lbNameXO.Text = "You are ";
+                pctbXO.Image = Image.FromFile(Application.StartupPath + "\\Resources\\X_caro.jpg");
+                pctbXO.BackgroundImageLayout = ImageLayout.Stretch;
+                pnlChessBeard.Enabled = true;
+
+            }
+            else
+            {
+                tmCoolDown.Stop();
+                prcbCoolDown.Value = 0;
+                socket.isServer = false;
+                pnlChessBeard.Enabled = false;
+                lbRole.Enabled = false;
+                lbRole.Text = "Guest";
+                lbNameXO.Text = "You are ";
+                pctbXO.Image = Image.FromFile(Application.StartupPath + "\\Resources\\O_caro.jpg");
+                pctbXO.BackgroundImageLayout = ImageLayout.Stretch;
+                //  socket.Send(new SocketData((int)SocketComand.CONNECT_SUCCESS, "", new Point()));
+                Listen();
+            }
+
+        }
+
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            txbIP.Text = socket.GetLocalIPv4(NetworkInterfaceType.Wireless80211);
+            if (string.IsNullOrEmpty(txbIP.Text))
+            {
+                txbIP.Text = socket.GetLocalIPv4(NetworkInterfaceType.Ethernet);
+            }
+        }
+
+        void Listen()
+        {
+
+            Thread listenThread = new Thread(() =>
+            {
+                try
+                {
+                    SocketData data = (SocketData)socket.Receive();
+                    ProcessData(data);
+                }
+                catch (Exception)
+                {
+
+                    throw;
+                }
+            });
+            listenThread.IsBackground = true;
+            listenThread.Start();
+
+
+        }
+
+        private void ProcessData(SocketData data)
+        {
+            switch (data.Command)
+            {
+                case (int)SocketComand.NOTIFY:
+                    MessageBox.Show(data.Message);
+                    break;
+                case (int)SocketComand.NEW_GAME:
+                    this.Invoke((MethodInvoker)(() =>
+                    {
+                        NewGame();
+                        pnlChessBeard.Enabled = false;
+                        lbRole.Enabled = false;
+                        lbRole.Text = "Guest";
+                        lbNameXO.Text = "You are ";
+                        pctbXO.Image = Image.FromFile(Application.StartupPath + "\\Resources\\O_caro.jpg");
+                        pctbXO.BackgroundImageLayout = ImageLayout.Stretch;
+                    }));
+                    break;
+                case (int)SocketComand.SEND_POINT:
+                    this.Invoke((MethodInvoker)(() =>
+                    {
+                        prcbCoolDown.Value = 0;
+                        pnlChessBeard.Enabled = true;
+                        tmCoolDown.Start();
+                        undoToolStripMenuItem.Enabled = true;
+                        ChessBoard.OtherPlayerMark(data.Point);
+                    }));
+                    break;
+                case (int)SocketComand.UNDO:
+                    Undo();
+                    prcbCoolDown.Value = 0;
+                    break;
+                case (int)SocketComand.END_GAME_WIN:
+                    prcbCoolDown.Value = 0;
+                    CustomMessageBox message = new CustomMessageBox("Bạn đã thắng!", Color.Green);
+                    message.ShowDialog();
+                    break;
+                case (int)SocketComand.END_GAME_LOSS:
+                    prcbCoolDown.Value = 0;
+                    CustomMessageBox message1 = new CustomMessageBox("Game Over! Bạn đã thua!", Color.BlueViolet);
+                    message1.ShowDialog();
+                    break;
+                case (int)SocketComand.TIME_OUT:
+                    MessageBox.Show("Hết giờ");
+                    break;
+                case (int)SocketComand.QUIT:
+                    tmCoolDown.Stop();
+                    MessageBox.Show("Đối thủ đã thoát");
+                    break;
+                case (int)SocketComand.CONNECT_SUCCESS:
+                    this.Invoke((MethodInvoker)(() =>
+                    {
+                        tmCoolDown.Stop();
+                        prcbCoolDown.Value = 0;
+                        lbLoading.Text = "Kết nối thành công";
+                    }));
+                    break;
+
+            }
+            Listen();
+        }
+        #endregion
+
+
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void pctbAvatar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pctbMark_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void prcbCoolDown_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txbPlayerName_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
